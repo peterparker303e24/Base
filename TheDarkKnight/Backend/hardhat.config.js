884a71cc0dfe8d3625d96dd84b315edd44f3a09b
PeterParker303e24 <peterparker303e24@proton.me>
2025-09-27 04:36:42 +0000

    Added .env and edited hardhat configurations.

--- TheDarkKnight/Backend/hardhat.config.js
+++ TheDarkKnight/Backend/hardhat.config.js
@@ -14,15 +14,24 @@ module.exports = {
       viaIR: true,
     }
   },
-  defaultNetwork: "sepolia", 
+  defaultNetwork: "localhost", 
   networks: {
-    hardhat: {
+    localhost: {
+      url: "http://localhost:8545"
       chainId: 1337
     },
-    sepolia: {
-      url: API_URL,
-      accounts: [PRIVATE_KEY],
-      gasPrice: 100_000_000
-    }
+
+    // To deploy contracts to the Sepolia blockchain:
+    // 1. Uncomment the lines below.
+    // 2. Change the "defaultNetwork" value above from "localhost" to "sepolia".    // 3. Obtain a URL endpoint to the Sepolia blockchain and input it to the API_URL value in the .env file.
+    // 4. With caution, input your Sepolia account private key into the PRIVATE_KEY value in the .env file.
+    // 5. Edit the "gasPrice" below to an appropriate value based on the network gas price demand.
+    // 6. Run the command "npx hardhat run scripts/deploy.js --network sepolia" to deploy the contracts to the Sepolia blockchain.
+    // 7. View the contract addresses in the output to search on the Sepolia blockchain with Sepolia EtherScan.
+    // sepolia: {
+    //   url: API_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   gasPrice: 100_000_000
+    // }
   }
 };
