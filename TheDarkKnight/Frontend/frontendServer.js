614bc80efdfd46c50213dab116d5b2f02ea7e76d
PeterParker303e24 <peterparker303e24@proton.me>
2025-09-28 22:53:48 +0000

    Updated configuration for easier connection to blockchains.

--- TheDarkKnight/Frontend/frontendServer.js
+++ TheDarkKnight/Frontend/frontendServer.js
@@ -1,9 +1,26 @@
-const express = require("express");
+import express from "express";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname));
+const inputArguments = process.argv;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+let isLocalBlockchain = false;
+if (inputArguments.includes("--local-blockchain")) {
+    isLocalBlockchain = true;
+}
+
+app.use(cookieParser());
+
+app.use(express.static(__dirname, {
+    setHeaders: function (res, path, stat) {
+        res.set('Set-Cookie', `isLocalBlockchain=${isLocalBlockchain};Path=/;SameSite=Strict`)
+    }
+}));
 
 app.use((req, res) => {
     res.status(404).sendFile(`${__dirname}/pages/404.html`);
